590c3a7186080f9bc8c6c9ceace8debd29b1a69c
psawant27 <psawant27feb1990@gmail.com>
2023-02-15 09:58:55 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,3 @@
 write a pyhton code to print number even number 1-200
+At least you try to attend all four question.
+You have to findout error through jenkins job. So atlease try to complete all other task.
