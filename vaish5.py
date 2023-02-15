8a0df3b9a34bb4ba0d6f7aafad47304fcdfa81ad
Vaishtanna1103 <95088682+Vaishtanna1103@users.noreply.github.com>
2023-02-15 10:20:37 +0530

    Update vaish5.py

--- vaish5.py
+++ vaish5.py
@@ -1,4 +1,6 @@
-for(x=0, x=500, x++)
-{if(x%2==0){print(x "is an even number")}
- else{print(x "is an odd number")}}
-break
+int eve, odd, x,
+for( x=0, x<=500, x++)
+if(x%2==0)
+even++
+else
+odd++
